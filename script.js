2ac886f342c2460ce529159211f5fdea4919feef
Alinka8 <alinazoldubaeva@gmail.com>
2023-04-15 07:55:06 -0700

    commit push

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const showModalBtn = document.querySelectorAll(".show-modal");
+const modalList = document.querySelectorAll(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(".close-modal");
+
+//add event listener to buttons
+const showModalHandler = (index) => {
+  modalList[index].classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModalHandler = (index) => {
+  modalList[index].classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+showModalBtn.forEach((el, index) => {
+  el.addEventListener("click", () => showModalHandler(index));
+});
+
+modalList.forEach((el, index) => {
+  el.addEventListener("click", (e) => {
+    closeModalHandler(index);
+  });
+});
+
+document.querySelector("body").addEventListener("keydown", (event) => {
+  console.log("keyboard clicked", event);
+  if (event.key === "Escape") {
+    modalList.forEach((el, index) => {
+      el.addEventListener("click", closeModalHandler(index));
+    });
+  }
+});
